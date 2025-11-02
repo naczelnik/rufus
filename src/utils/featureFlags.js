b9696ce922b5d359c51fe04a32fce091cb7e6495
naczelnik <naczelnik@gmail.com>
2025-11-02 01:43:04 +0100

    poprawka 2

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -1,4 +1,20 @@
-// src/utils/featureFlags.js
+/**
+ * Centralny obiekt do zarządzania flagami funkcyjnymi w aplikacji.
+ * Pozwala na włączanie/wyłączanie poszczególnych funkcjonalności.
+ * 
+ * Przykład użycia:
+ * import { featureFlags } from './utils/featureFlags';
+ * 
+ * if (featureFlags.authEnabled) {
+ *   // Uruchom kod związany z autoryzacją
+ * }
+ */
 export const featureFlags = {
+    /**
+     * @type {boolean}
+     * Włącza/wyłącza cały system autoryzacji i logowania oparty na Supabase.
+     * Ustaw na `false`, aby aplikacja działała w trybie publicznym (jak wcześniej).
+     * Ustaw na `true`, aby wymagać logowania od użytkowników.
+     */
     authEnabled: true,
 };
